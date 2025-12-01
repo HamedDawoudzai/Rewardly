c188db3b6bb0b5974bcd709ea93a055d1a8dcbd8
BrandanBurgess <brandanbb13@gmail.com>
2025-11-30 23:56:19 -0500

    feat: protect role based routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { DashboardLayout } from '@/components/layout'
+import { ProtectedRoute } from '@/components/auth'
 import { AuthProvider } from '@/context/AuthContext'
 import LandingPage from '@/components/LandingPage'
 
@@ -63,34 +64,98 @@ function App() {
             <Route path="/redeem" element={<RedeemPage />} />
             <Route path="/redemption/:id" element={<RedemptionQRPage />} />
             
-            {/* Cashier Routes */}
-            <Route path="/cashier/transactions" element={<CreateTransactionPage />} />
-            <Route path="/cashier/redemptions" element={<ProcessRedemptionPage />} />
+            {/* Cashier Routes - Requires cashier role or higher */}
+            <Route path="/cashier/transactions" element={
+              <ProtectedRoute requiredRole="cashier">
+                <CreateTransactionPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/cashier/redemptions" element={
+              <ProtectedRoute requiredRole="cashier">
+                <ProcessRedemptionPage />
+              </ProtectedRoute>
+            } />
             
-            {/* Event Organizer Routes */}
+            {/* Event Organizer Routes - Available to all authenticated users */}
             <Route path="/organizer/events" element={<MyEventsPage />} />
             <Route path="/organizer/events/:id" element={<EventDetail />} />
             <Route path="/organizer/events/:id/edit" element={<OrganizerEditEventPage />} />
             <Route path="/organizer/events/:id/guests" element={<ManageGuestsPage />} />
             <Route path="/organizer/events/:id/award" element={<AwardPointsPage />} />
             
-            {/* Manager Routes */}
-            <Route path="/manager/users" element={<UsersManagementPage />} />
-            <Route path="/manager/users/:id" element={<UserDetailPage />} />
-            <Route path="/manager/transactions" element={<AllTransactionsPage />} />
-            <Route path="/manager/transactions/:id" element={<TransactionDetail />} />
-            <Route path="/manager/promotions" element={<PromotionsManagementPage />} />
-            <Route path="/manager/promotions/new" element={<CreatePromotionPage />} />
-            <Route path="/manager/promotions/:id" element={<PromotionDetail />} />
-            <Route path="/manager/promotions/:id/edit" element={<CreatePromotionPage />} />
-            <Route path="/manager/events" element={<EventsManagementPage />} />
-            <Route path="/manager/events/new" element={<CreateEventPage />} />
-            <Route path="/manager/events/:id" element={<EventDetail />} />
-            <Route path="/manager/events/:id/edit" element={<CreateEventPage />} />
-            <Route path="/manager/events/:id/attendees" element={<EventDetail />} />
+            {/* Manager Routes - Requires manager role or higher */}
+            <Route path="/manager/users" element={
+              <ProtectedRoute requiredRole="manager">
+                <UsersManagementPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/users/:id" element={
+              <ProtectedRoute requiredRole="manager">
+                <UserDetailPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/transactions" element={
+              <ProtectedRoute requiredRole="manager">
+                <AllTransactionsPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/transactions/:id" element={
+              <ProtectedRoute requiredRole="manager">
+                <TransactionDetail />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/promotions" element={
+              <ProtectedRoute requiredRole="manager">
+                <PromotionsManagementPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/promotions/new" element={
+              <ProtectedRoute requiredRole="manager">
+                <CreatePromotionPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/promotions/:id" element={
+              <ProtectedRoute requiredRole="manager">
+                <PromotionDetail />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/promotions/:id/edit" element={
+              <ProtectedRoute requiredRole="manager">
+                <CreatePromotionPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/events" element={
+              <ProtectedRoute requiredRole="manager">
+                <EventsManagementPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/events/new" element={
+              <ProtectedRoute requiredRole="manager">
+                <CreateEventPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/events/:id" element={
+              <ProtectedRoute requiredRole="manager">
+                <EventDetail />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/events/:id/edit" element={
+              <ProtectedRoute requiredRole="manager">
+                <CreateEventPage />
+              </ProtectedRoute>
+            } />
+            <Route path="/manager/events/:id/attendees" element={
+              <ProtectedRoute requiredRole="manager">
+                <EventDetail />
+              </ProtectedRoute>
+            } />
             
-            {/* Superuser/Admin Routes */}
-            <Route path="/admin/users" element={<UserRolesPage />} />
+            {/* Superuser/Admin Routes - Requires superuser role */}
+            <Route path="/admin/users" element={
+              <ProtectedRoute requiredRole="superuser">
+                <UserRolesPage />
+              </ProtectedRoute>
+            } />
           </Route>
           
           {/* Catch-all redirect */}
